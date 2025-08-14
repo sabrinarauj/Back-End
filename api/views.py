e29c570af16732faf6d2f754a1add73d262cbe66
unknown <ds@ESN501D1278471.educ501.local>
2025-08-14 16:07:49 -0300

    atualizações feitas na aula

--- api/views.py
+++ api/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from .models import Autor
 from .serializers import AutorSerializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 class AutoresView(ListCreateAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializers
+
+@app_view(['GET', 'POST'])
+def visualizacao_autor(request):
+    if request.method == 'GET':
+        queryset = Autor.objects.all()
+        serializer = AutorSerializers(queryset, many = True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = AutorSerializers(data = request.data)  
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
