49b3e0b5232a731a2db010b0aa5202bb87243392
sabrinarauj <sabrina65bina@gmail.com>
2025-08-21 16:57:56 -0300

    atualizações 21/08

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import AutoresView, visualizacao_autor, EditorasView, LivrosView
+from .views import *
 
 urlpatterns = [
     path('autores', AutoresView.as_view()),
     path('authors', visualizacao_autor),
     path('editoras', EditorasView.as_view()),
     path('livros', LivrosView.as_view()),
-    
+
+
+    path('autor/<int:pk>', AutoresDetailView.as_view()),
+    path('editora/<int:pk>', EditorasDetailView.as_view()),
+    path('livro/<int:pk>', LivrosDetailView.as_view()),
 ]
